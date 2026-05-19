42c29002146f10d4f8755c4dad59f4f1068e9ddf
Fernando Leonid <fernandoleonid@gmail.com>
2026-05-19 20:25:51 -0300

    exemplo 01  e 02

---
+++ live01/exemplo01.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+cidade = input('Digite sua cidade: ')
+idade = input('Digite sua idade: ')
+altura = input('Digite sua altura: ')
+mensagem = f'{nome} é da cidade de {cidade} tem {idade} anos e altura de {altura}'
+
+print (mensagem)
---
+++ live01/exemplo02.py
@@ -0,0 +1,6 @@
+ano_atual = int(input ('Digite o ano atual: '))
+ano_nascimento = int(input('Digite o seu ano de nascimento: ') )
+
+idade = ano_atual - ano_nascimento
+
+print (idade)
