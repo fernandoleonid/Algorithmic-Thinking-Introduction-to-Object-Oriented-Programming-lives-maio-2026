9dcef1393d0423d430b06d3f175b05c8992714a6
Fernando Leonid <fernandoleonid@gmail.com>
2026-05-19 20:37:36 -0300

    exemplo03

---
+++ live01/exemplo03.py
@@ -0,0 +1,12 @@
+idade = int(input ('Digite quantos anos de trabalho: '))
+
+# junior até 3
+# pleno até 5
+# senior maiores 5 anos 
+
+if idade <= 3:
+    print ('Junior')
+elif idade  <=5:
+    print ('Pleno')
+else:
+    print ('Senior')
