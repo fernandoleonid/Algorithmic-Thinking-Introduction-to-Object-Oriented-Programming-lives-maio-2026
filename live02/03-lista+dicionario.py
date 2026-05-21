ab795ca0d74e161512cd679a1b5ff2db34307c67
Fernando Leonid <fernandoleonid@gmail.com>
2026-05-21 20:14:35 -0300

    live02 - funções

---
+++ live02/03-lista+dicionario.py
@@ -0,0 +1,25 @@
+alunos = [
+    {
+        'nome': 'Ana',
+        'nota': 9,
+        'semestre': 4
+    },
+    {
+        'nome': 'Pedro',
+        'nota': 2,
+        'semestre': 1
+    },
+    {
+        'nome': 'Maria',
+        'nota': 7,
+        'semestre': 2
+    }
+]
+
+alunos.append ( {
+    'nome': 'Hugo',
+    'nota': 3,
+    'semestre': 7
+})
+
+print (alunos)
---
+++ live02/04-funcao.py
@@ -0,0 +1,7 @@
+def mensagem(): 
+    print ('Olá Turma!')
+
+def somar (a, b):
+    return a + b
+
+print(somar (7, 8)) 
